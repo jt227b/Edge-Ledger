b1f8a6924016b5b1beca8ca15ca5d79086370a79
Jose Torres <jt227b@gmail.com>
2019-07-24 13:37:30 -0500

    Added JS and JQuery to scroll

--- js/main.js
+++ js/main.js
@@ -11,3 +11,30 @@ function initMap() {
      // The marker, positioned at location
      const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+
+// Sticky menu background
+window.addEventListener('scroll', function () {
+     if (window.scrollY > 150) {
+          document.querySelector('#navbar').style.opacity = 0.9;
+     } else {
+          document.querySelector('#navbar').style.opacity = 1;
+     }
+});
+
+
+// Smooth Scrolling code from JQuery CDN
+$('#navbar a, .btn').on('click', function (event) {
+     if (this.hash !== '') {
+          event.preventDefault();
+
+          const hash = this.hash;
+
+          $('html, body').animate(
+               {
+                    scrollTop: $(hash).offset().top - 100
+               },
+               800
+          );
+     }
+});
